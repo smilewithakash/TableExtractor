4eddd9eecf1a609ec207ab2a04eac7b2be0c3904
Akash Deep <48481250+smilewithakash@users.noreply.github.com>
2023-08-03 20:07:09 +0530

    Add files via upload

---
+++ Table Extractor.py
@@ -0,0 +1,30 @@
+import os
+import camelot
+import pandas as pd
+
+# First, try to extract tables without performing OCR
+tables = camelot.read_pdf('Presentation2.pdf', flavor='stream', pages="1-end")
+
+# If no tables are found, perform OCR and try again
+if len(tables) == 0:
+    os.system("ocrmypdf Presentation2.pdf output.pdf")
+    tables = camelot.read_pdf('output.pdf', flavor='stream', pages="1-end")
+
+# If tables are still not found, print a message and exit
+if len(tables) == 0:
+    print('No tables found in document.')
+    exit()
+
+# Create a Pandas Excel writer using XlsxWriter as the engine.
+writer = pd.ExcelWriter('output.xlsx', engine='openpyxl')
+
+# Loop over the tables and write each one to a separate sheet in the Excel file
+for i, table in enumerate(tables):
+    # Convert the table to a pandas DataFrame
+    df = table.df
+
+    # Write the DataFrame to an Excel file
+    df.to_excel(writer, sheet_name=f'Table {i}', index=False)
+
+# Close the Pandas Excel writer and output the Excel file.
+writer.save()
